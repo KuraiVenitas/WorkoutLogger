02bbdf1e7ddf32fb4e75cd09bed6e16cfc1d1a25
KuraiVenitas <141559029+KuraiVenitas@users.noreply.github.com>
2026-01-28 18:45:46 -0500

    Added Basic functionality that interacts with endpoints

--- main.py
+++ main.py
@@ -1,9 +1,59 @@
 from fastapi import FastAPI
+from pydantic import BaseModel #Import BaseModel to check types automatically, Reinforces required vs optional fields
+from datetime import date #Import the library for dates. Prevents invalid dates
+from typing import Optional #Allows for some fields to be missing
+import uuid
 
+#----------------Variables----------------#
 app = FastAPI()
+workoutdict = {} #Holds workout logs
 
-@app.get("/")
+#----------------Classes----------------#
+#WorkoutCreate Class includes three fields: Date, Calories burned and Workout Length
+class WorkoutCreate(BaseModel):
+        date: date
+        calories_burned: Optional[int] = None
+        length_minutes: Optional[int] = None
 
+#Workout Class includes the addition of a UID
+class Workout(BaseModel):
+        id: str
+        date: date
+        calories_burned: Optional[int] = None
+        length_minutes: Optional[int] = None
+
+#----------------Endpoints----------------#
 @app.get("/")
 def root():
         return {"status": "ok"}
+
+#Return all workouts
+@app.get("/workouts/")
+def getWorkout():
+        return workoutdict
+
+@app.get("/workouts/{id}")
+def getWorkout(id: str):
+        return workoutdict[id]
+
+@app.post("/workouts/")
+def create_workout(workout_in: WorkoutCreate):
+        workout = add_uuid(workout_in) # Returns a workout object
+        add_workout_to_dict(workout)
+        return workout
+        
+
+
+#---------------------------Functions-------------------------#
+#Creates a UUID and assigns it to the new workout
+def add_uuid(workout_in: WorkoutCreate) -> Workout:
+        return Workout(
+                id = str(uuid.uuid4()),
+                date = workout_in.date,
+                calories_burned = workout_in.calories_burned,
+                length_minutes = workout_in.length_minutes
+                )
+
+#Adds the workout to a workout dictionary
+def add_workout_to_dict(workout_in:Workout):
+        workoutdict.update({workout_in.id : workout_in})
